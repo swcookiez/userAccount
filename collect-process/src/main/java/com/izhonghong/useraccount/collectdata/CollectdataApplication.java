9e0a771ebf26655762b3185dd4671b8e200d79d0
sw <swcookiez@163.com>
2021-06-16 18:25:35 +0800

    本地git提交

---
+++ collect-process/src/main/java/com/izhonghong/useraccount/collectdata/CollectdataApplication.java
@@ -0,0 +1,13 @@
+package com.izhonghong.useraccount.collectdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectdataApplication.class, args);
+    }
+
+}
