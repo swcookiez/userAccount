9dc1a2de0c916006b6296d4572f3f7940cfa9845
sw <swcookiez@163.com>
2021-06-19 14:09:37 +0800

    信息源管理平台驱动类

---
+++ informationmanager/src/main/java/com/izhonghong/informationmanager/InformationmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.izhonghong.informationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationmanagerApplication.class, args);
+    }
+
+}
