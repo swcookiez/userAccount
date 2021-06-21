bf1fd9140caead32df5805f5a53ae4d9d81d10d9
sw <swcookiez@163.com>
2021-06-21 09:28:41 +0800

    APP

---
+++ task-hdfs-picture-readwrite/src/main/java/com/izhonghong/temporarytask/taskhdfspicturereadwrite/TaskHdfsPictureReadwriteApplication.java
@@ -0,0 +1,13 @@
+package com.izhonghong.temporarytask.taskhdfspicturereadwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHdfsPictureReadwriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskHdfsPictureReadwriteApplication.class, args);
+    }
+
+}
