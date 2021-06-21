0c03d8e292a461bb2d986f5f0a306b7c495d972b
sw <swcookiez@163.com>
2021-06-21 09:27:13 +0800

    hdfs 常量

---
+++ task-hdfs-picture-readwrite/src/main/java/com/izhonghong/temporarytask/taskhdfspicturereadwrite/common/HDFSConstant.java
@@ -0,0 +1,11 @@
+package com.izhonghong.temporarytask.taskhdfspicturereadwrite.common;
+
+public class HDFSConstant {
+   //HDFS写入图片的地址
+   public static final String HDFS_PICTURE_PATH = "/PictureReadWrite/";
+   //HDFS地址
+   public static final String HDFS_ADDRESS = "hdfs://test113:8020";
+
+   public static final String HDFS_USER = "zhonghong";
+
+}
