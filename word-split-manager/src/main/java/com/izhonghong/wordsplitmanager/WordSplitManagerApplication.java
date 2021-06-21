7870f1670c904b6c27efe8827456e268c47f7ff5
sw <swcookiez@163.com>
2021-06-21 10:32:19 +0800

    app

---
+++ word-split-manager/src/main/java/com/izhonghong/wordsplitmanager/WordSplitManagerApplication.java
@@ -0,0 +1,13 @@
+package com.izhonghong.wordsplitmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSplitManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordSplitManagerApplication.class, args);
+    }
+
+}
