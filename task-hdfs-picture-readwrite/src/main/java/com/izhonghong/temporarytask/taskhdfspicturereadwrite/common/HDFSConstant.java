c83d9b08c5f30a1dbc44a9caa2526695882cd91d
sw <swcookiez@163.com>
2021-07-23 13:13:50 +0800

    访问用户

--- task-hdfs-picture-readwrite/src/main/java/com/izhonghong/temporarytask/taskhdfspicturereadwrite/common/HDFSConstant.java
+++ task-hdfs-picture-readwrite/src/main/java/com/izhonghong/temporarytask/taskhdfspicturereadwrite/common/HDFSConstant.java
@@ -5,7 +5,7 @@ public class HDFSConstant {
    public static final String HDFS_PICTURE_PATH = "/PictureReadWrite/";
    //HDFS地址
    public static final String HDFS_ADDRESS = "hdfs://test113:8020";
-
+   //访问用户
    public static final String HDFS_USER = "zhonghong";
 
 }
